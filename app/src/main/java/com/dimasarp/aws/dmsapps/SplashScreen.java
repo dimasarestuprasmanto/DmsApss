93907e43f9e1f2fa67ead27449b2d55d0ada67b1
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2020-05-04 01:02:12 +0700

    Splash Screen and Walkthrough

---
+++ app/src/main/java/com/dimasarp/aws/dmsapps/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.dimasarp.aws.dmsapps;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, IntroActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
